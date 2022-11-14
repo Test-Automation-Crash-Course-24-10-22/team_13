ef36a04547e108ff7cd0f6cf7f59083cb281fdc9
Denis Pitsul <denia.pitsul@gmail.com>
2022-11-14 17:56:40 +0200

    Init java maven, load rozetka.com.ua

---
+++ src/test/java/org/team13/RozetkaTests.java
@@ -0,0 +1,26 @@
+package org.team13;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class RozetkaTests {
+
+    private WebDriver driver;
+
+    @Test
+    public void loadRozetkaPage() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rozetka.com.ua/");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+}
